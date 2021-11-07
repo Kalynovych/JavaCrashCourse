b996326c056581ee9af73952a2dabb5cf933aafe
Kalynovych <92263512+Kalynovych@users.noreply.github.com>
2021-11-07 22:56:06 +0200

    Update Defender.java

--- untitled/src/wargame/Defender.java
+++ untitled/src/wargame/Defender.java
@@ -6,6 +6,7 @@ public class Defender extends Warrior {
         super(60, 3);
         defence = 2;
     }
+    
     @Override
     public int takeDamage(Warrior attacker) {
         int damage = attacker.getAttack();
